385b4a1e8ed02c1d5b5f33f1d3baf1604b3a9026
hao-pt <31931630+hao-pt@users.noreply.github.com>
2026-05-10 21:51:50 +0000

    Deploying to gh-pages from @ hao-pt/hao-pt.github.io@6fc182a86fc45d30324ef3ab90662e5455807e60 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,21 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "post-a-post-with-plotly-js",
+  },{id: "nav-news",
+          title: "news",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/news/";
+          },
+        },{id: "nav-publications",
+          title: "publications",
+          description: "(*) denotes equal contribution",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
